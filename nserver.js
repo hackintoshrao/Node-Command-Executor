1a1824ef3a7a16dd5db87a212a69f17d54fbbbd5
Karthic Rao <kartronics85@gmail.com>
2013-02-23 06:34:19 +0530

    Command executor webserver getting prepared

--- nserver.js
+++ nserver.js
@@ -1,62 +1,24 @@
-var http=require('http'),
-	url = require('url'),
-	mime = require('mime'),//npm install mime
-	fs = require('fs'),
-	search_path='/home/karthic/mydata/codes';
-http.createServer(function(req,res) {
-	 pathname = search_path + req.url;
-	 console.log(req.url);
-	 if(req.url == '/ls' ){
-                var spawn = require('child_process').spawn,
-		ls = spawn('ls');
-		ls.stdout.on('data', function (data) {
-		console.log('stdout: ' + data);
-		res.writeHead(200);
-		res.write('Getting the Directory listing.....');
-		res.write(data);
-		res.end();
-		
-		});
-		ls.stderr.on('data', function (data) {
-		console.log('stderr: ' + data);
-		});
-ls.on('exit', function (code) {
-console.log('child process exited with code ' + code);
-	});
-		
-	}
-	 console.log(pathname);
+var http = require('http');
+var postHTML = 
+  '<html><head><title>Post Example</title></head>' +
+  '<body>' +
+  '<form method="post">' +
+  'Input 1: <input name="input1"><br>' +
+  'Input 2: <input name="input2"><br>' +
+  '<input type="submit">' +
+  '</form>' +
+  '</body></html>';
 
-	fs.stat(pathname , function(err,stats){
-	  if(err) { 
-		res.writeHead(404);
-		res.write('Bad Request , Request cannot be accepted');
-		res.end()
-		}
-	else if(stats.isFile()) {
-		//content-type
-		var type = mime.lookup(pathname);
-		console.log(type);
-		res.setHeader('Content-Type',type);
+http.createServer(function (req, res) {
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+  req.on('end', function () {
+    console.log('POSTed: ' + body);
+    res.writeHead(200);
+    res.end(postHTML);
+  });
+}).listen(8080);
 
-		// 200 status - found , no errors 
-		res.statusCode = 200;
-	
-		//create a readable stream and pipe to the http response 
-		var file = fs.createReadStream(pathname);
-		file.on("open",function() { 
-			file.pipe(res);
-		});
-		file.on("error",function(err) {
-			cosole.log(err);
-		});
-	     }
-	else {
-		res.writeHead(403);
-		res.write('Cannot Access Directory');
-		res.end();
-		}
-	});
-}).listen(8124);
-console.log('Server running at 8124');
-		
+
