68b914b7ca6a1046c92cf09e5d50699801ecbe64
Karthic Rao <kartronics85@gmail.com>
2013-02-26 22:40:18 +0530

    The executor added

--- nserver.js
+++ nserver.js
@@ -19,3 +19,16 @@ http.createServer(function(req, res) {
     console.log(err);
   });
 }).listen(8080);
+
+//The executor part 
+var exec = require('child_process').exec,
+    child;
+
+child = exec('cat *.js bad_file | wc -l',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
